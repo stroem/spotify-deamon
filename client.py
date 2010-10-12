f50b3c2f40f5bd2fa765f57c93dda85c6ddcce11
Jonathan Ström <jonathan.strom@gmail.com>
2010-10-12 12:58:21 +0200

    disabled config login

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import ConfigParser
 import optparse
 from session_manager import *
 
@@ -9,16 +8,9 @@ op.add_option("-u", "--username", help="spotify username")
 op.add_option("-p", "--password", help="spotify password")
 (options, args) = op.parse_args()
 
-username = options.username
-password = options.password
-
 if not options.username or not options.password:
-	config = ConfigParser.ConfigParser()
-	config.read("client.conf")
-	username = config.get("spotify", "username")
-	password = config.get("spotify", "password")
-	#op.print_help()
-	#raise SystemExit
+	op.print_help()
+	raise SystemExit
 	
-s = SessionManager(username, password)
+s = SessionManager(options.username, options.password)
 s.connect()
